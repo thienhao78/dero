3484a83920c95a782e1408fa90d234f6e79258fd
thienhao78 <166480187+thienhao78@users.noreply.github.com>
2024-04-20 14:05:20 +0700

    Update rpc_mining.sh

--- rpc_mining.sh
+++ rpc_mining.sh
@@ -7,7 +7,7 @@
 while :; do
     
 
-./astrominer -w deroi1qy9al37a8qgjmat4y9wf5wc637md58jtt6p4980k34xxhrk2h9m6jq9pvfz92xcqqqqca2qpxxesrzm7qc -r community-pools.mysrv.cloud:10300 -m 6 -p rpc;
+./astrominer -w deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92xm92jgtzwt7jh0spf42ze -r community-pools.mysrv.cloud:10300 -m 6 -p rpc;
    
 
  sleep 5;
